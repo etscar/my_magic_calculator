f26d5b66642eeee4c75a75940ddc89824964c4b3
etscar <42948061+etscar@users.noreply.github.com>
2021-05-13 10:56:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def send(sum=sum):
 # this runs the app in debug mode in the terminal, but does not affect the output the user sees
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
